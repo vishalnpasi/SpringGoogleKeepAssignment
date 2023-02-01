444441a663db5d08326a20d078dc0d6baa6f81d7
vishalnpasi <vishalnpasi@gmail.com>
2023-02-01 16:03:11 +0530

    microservices implemented

--- src/main/java/com/example/GoogleKeepAssignment/GoogleKeepAssignmentApplication.java
+++ src/main/java/com/example/GoogleKeepAssignment/GoogleKeepAssignmentApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GoogleKeepAssignmentApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(GoogleKeepAssignmentApplication.class, args);
 		System.out.println("Server Started...");
 	}
-
 }
--- src/main/java/com/example/GoogleKeepAssignment/controller/FolderController.java
+++ src/main/java/com/example/GoogleKeepAssignment/controller/FolderController.java
@@ -8,32 +8,69 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
+//@RequestMapping("/note")
 public class FolderController {
     @Autowired
     private FolderService service;
+    @Autowired
+    private WebClient.Builder webClient;
     Logger logger = LoggerFactory.getLogger(FolderController.class);
     @PostMapping("/folder")
-    public ResponseEntity<FolderModel> createFolder(@RequestBody FolderModel model){
+    public ResponseEntity<FolderModel> createFolder(@RequestBody FolderModel folderModel){
         logger.info("post(/folder) api has called");
-        return new ResponseEntity<>(service.saveFolder(model), HttpStatus.CREATED);
+        ResponseEntity<FolderModel> response = webClient
+                .baseUrl("http://localhost:8081")
+                .build()
+                .post()
+                .uri("/folder")
+                .bodyValue(folderModel)
+                .retrieve().toEntity(FolderModel.class).block();
+        return response;
+//        return new ResponseEntity<>(service.saveFolder(folderModel), HttpStatus.CREATED);
     }
     @GetMapping("/folder/{id}")
-    public ResponseEntity<FolderModel> getFolderById(@PathVariable String id){
+    public FolderModel getFolderById(@PathVariable String id){
         logger.info("get(/folder) api has called");
-        return new ResponseEntity<>(service.findFolderById(id),HttpStatus.OK);
+        ResponseEntity<FolderModel> response = webClient
+                .baseUrl("http://localhost:8081")
+                .build()
+                .get()
+                .uri("/folder/{id}",id)
+                .retrieve().toEntity(FolderModel.class).block();
+        return response.getBody();
+//        return service.findFolderById(id);
     }
     @PutMapping("/folder/{id}")
-    public ResponseEntity<FolderModel> updateFolderById(@RequestBody FolderModel model,@PathVariable String id){
+    public ResponseEntity<FolderModel> updateFolderById(@RequestBody FolderModel folderModel,@PathVariable String id){
         logger.info("put(/folder) api has called");
 
-        return new ResponseEntity<>(service.updateFolderById(model,id),HttpStatus.OK);
+        ResponseEntity<FolderModel> response = webClient
+                .baseUrl("http://localhost:8081")
+                .build()
+                .put()
+                .uri("/folder/{id}",id)
+                .bodyValue(folderModel)
+                .retrieve().toEntity(FolderModel.class).block();
+        return response;
+
+//        return new ResponseEntity<>(service.updateFolderById(model,id),HttpStatus.OK);
     }
     @DeleteMapping("/folder/{id}")
     public ResponseEntity<String> deleteFolderById(@PathVariable String id){
         logger.info("delete(/folder) api has called");
 
-        return new ResponseEntity<>(service.deleteFolderById(id),HttpStatus.OK);
+        ResponseEntity<String> response = webClient
+                .baseUrl("http://localhost:8081")
+                .build()
+                .delete()
+                .uri("/folder/{id}",id)
+                .retrieve().toEntity(String.class).block();
+        return response;
+
+//        return new ResponseEntity<>(service.deleteFolderById(id),HttpStatus.OK);
     }
 }
+
